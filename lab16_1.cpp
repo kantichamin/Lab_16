39d70fc8631b668d928a4da4166179468b6a1037
Kanticha <kantian.0633717109@gmail.com>
2026-02-04 02:10:45 +0700

    name

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr + i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr + i) = (rand()%101)*0.01;
+    }
+}
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N*M; j++){
+            *(sum1+i) += *(dPtr+j);
+            if((j+1)%M==0) i++;
+        }
+    }
+}
+
+void findColSum(const double *dPtr,double *sum2,int N,int M){
+    for(int i=0; i<N*M; i++){
+        for(int j=0; j<N; j++){
+            for(int z=0; z<M; z++){
+                *(sum2+z) += *(dPtr+i);
+                i++;
+            }
+        }
+    }
+}
